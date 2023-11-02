8fa799b1496ee8c0c38d76e2b298a9091925be11
suhailsinghbains <bains.singh.suhail@gmail.com>
2023-11-02 16:17:28 -0400

    Initial Commit

---
+++ testCounter/testCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  testCounterApp.swift
+//  testCounter
+//
+//  Created by Suhail Singh Bains on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct testCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
